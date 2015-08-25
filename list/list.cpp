6114197a12a06e3768f140ecc51ca8f29524cb86
Jiraphat <lesaku--like@hotmail.com>
2015-08-26 02:11:17 +0700

    completed

--- list/list.cpp
+++ list/list.cpp
@@ -21,6 +21,17 @@ void List::pushToHead(char el)
 void List::pushToTail(char el)
 {
 	//TO DO!
+	Node *tmp = new Node(el, NULL);
+	if (isEmpty())
+	{
+		head = tmp;
+		tail = tmp;
+	}
+	else
+	{
+		tail->next = tmp;
+		tail = tmp;
+	}
 }
 char List::popHead()
 {
@@ -40,16 +51,56 @@ char List::popHead()
 char List::popTail()
 {
 	// TO DO!
-	return NULL;
+	char el = tail->data;
+	Node *tmp = head;
+	if (head == tail)
+	{
+		head = tail = 0;
+	}
+	else
+	{
+		while (tmp->next->next != NULL)
+			tmp = tmp->next;
+		delete tail;
+		tail = tmp;
+		tmp->next = NULL;
+	}
+	return el;
 }
 bool List::search(char el)
 {
 	// TO DO! (Function to return True or False depending if a character is in the list.
-	return NULL;
+	Node *tmp = head;
+	while (tmp != NULL)
+	{
+		if (tmp->data == el)
+			return true;
+		tmp = tmp->next;
+	}
+	return false;
 }
 void List::reverse()
 {
 	// TO DO! (Function is to reverse the order of elements in the list.
+	Node *tmp1 = head;
+	Node *tmp2 = tail;
+	while (tmp2 != head)
+	{
+		tmp1 = head;
+		while (tmp1 != tmp2)
+		{
+			if (tmp1->next == tmp2)
+			{
+				tmp2->next = tmp1;
+				tmp2 = tmp2->next;
+				break;
+			}
+			tmp1 = tmp1->next;
+		}
+	}
+	head = tail;
+	tail = tmp2;
+	tmp2->next = NULL;
 }
 void List::print()
 {
